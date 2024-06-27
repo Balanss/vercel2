efd669dd7a8bd5dbec284b8255323dcd934f92a9
Balanss <gilmonpurry@hotmail.com>
2024-06-27 20:43:08 -0300

    chore: Update CORS origin and enable credentials in server/index.js

--- server/index.js
+++ server/index.js
@@ -4,22 +4,22 @@ import express from "express";
 
 const app = express();
 
+app.use(cors({
+    origin: "https://vercel2-frontend.vercel.app", // Add the correct origin here
+    methods: ["GET", "POST"], // Adjust methods according to your needs
+    credentials: true // Enable credentials
+  }));
+  
 
 app.use(express.json({limit: "10mb"}));
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
+
+
 
 app.get("/", (req, res) => {
     res.json("Hello Vite + React!");
 });
 
-app.use(cors({
-  origin: "https://vercel2-frontend.vercel.app", // Add the correct origin here
-  methods: ["GET", "POST"], // Adjust methods according to your needs
-  credentials: true // Enable credentials
-}));
 
   app.get("/hi", (req, res) => {
     res.json({ message: "yaya" });
